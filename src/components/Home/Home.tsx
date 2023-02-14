0f1d01c4e48a0568c88322242cea1e3199086dc6
= <=>
2023-02-14 12:58:24 -0700

    working on home and table layout

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import './Home.css'
 
 function Home() {
-    return(
-        <div className="main-container">Home</div>
-    )
+  return(
+    <div className="main-container">
+      <h2 className="heading">Squat load this dynamically</h2>
+      <h3 className="heading">Current e1rm: inject here</h3>
+    </div>
+  )
 }
 
 export default Home;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Table.css';
+
+function Table() {
+  return(
+    <>
+      
+    </>
+  )
+}
+
+export default Table;
