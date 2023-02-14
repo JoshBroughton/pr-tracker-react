805de6b8806cb692b52e8a44df9e5636c671627f
= <=>
2023-02-14 13:14:32 -0700

    setting up ag-grid table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,10 +1,32 @@
-import React from "react";
-import './Table.css';
+import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+interface LiftRecord {
+  reps: number;
+  weight: number;
+  date: Date;
+  e1rm: number;
+}
+
+const data: LiftRecord[] = [
+  {
+    reps: 1,
+    weight: 445,
+    date: new Date(),
+    e1rm: 445, 
+  }
+]
 
 function Table() {
+  const [rowData, setRowData] = useState<LiftRecord[]>(data);
+
   return(
     <>
-      
+
     </>
   )
 }
