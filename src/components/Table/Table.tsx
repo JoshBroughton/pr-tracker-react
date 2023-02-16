d241a11abb3fba67369891ce645307ab7f9031af
= <=>
2023-02-16 12:23:15 -0700

    switching to tailwindcss for table stying

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,9 +1,6 @@
 import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
 import { render } from 'react-dom';
-import { AgGridReact } from 'ag-grid-react';
 
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 interface LiftRecord {
   reps: number;
