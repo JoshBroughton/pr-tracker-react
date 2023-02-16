adf494e7c9004636e003dbd8b057b96661946fd9
= <=>
2023-02-16 13:04:26 -0700

    leaving tailwind aside for now, added simple styling to table, mapping data from array of lift objects

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
 import { render } from 'react-dom';
+import "./Table.css";
 
 
 interface LiftRecord {
@@ -15,16 +16,42 @@ const data: LiftRecord[] = [
     weight: 445,
     date: new Date(),
     e1rm: 445, 
+  },
+  {
+    reps: 2,
+    weight: 405,
+    date: new Date(),
+    e1rm: 435,
   }
 ]
 
 function Table() {
   const [rowData, setRowData] = useState<LiftRecord[]>(data);
 
-  return(
-    <>
+  function mapRow(row:LiftRecord):React.ReactElement {
+    return(
+      <tr>
+        <td>{row.reps}</td>
+        <td>{row.weight}</td>
+        <td>{row.e1rm}</td>
+        <td>{row.date.toDateString()}</td>
+      </tr>
+    )
+  };
+  let rows = rowData.map(mapRow);
 
-    </>
+  return(
+    <table className='table-auto'>
+      <thead>
+        <th>Reps</th>
+        <th>Weight</th>
+        <th>Estimated 1-Rep Max</th>
+        <th>Date</th>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
   )
 }
 
