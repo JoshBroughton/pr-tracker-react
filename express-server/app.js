8557dedb12450ac963f268f8632a00f1c01d3b6e
= <=>
2023-02-17 10:54:12 -0700

    started API validation

--- express-server/app.js
+++ express-server/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const { auth } = require('express-oauth2-jwt-bearer');
 const port = 4000;
+
+const jwtCheck = auth({
+  audience: 'https://pr-tracker-api-endpoint',
+  issuerBaseURL: 'https://dev-51h0rrt38nuipv61.us.auth0.com/',
+  tokenSigningAlg: 'RS256'
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+app.get
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
