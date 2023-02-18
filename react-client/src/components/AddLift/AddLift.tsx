fc26ae0ac576b8ee3bc32112a19827276007a1a3
= <=>
2023-02-18 14:44:22 -0700

    AddLift component is succesfully posting form data to the server

--- react-client/src/components/AddLift/AddLift.tsx
+++ react-client/src/components/AddLift/AddLift.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 
 
 function AddLift() {
-  const [text, setText] = useState<string>('');
+  const [reps, setReps] = useState<number>(1);
   const [message, setMessage] = useState<string>('');
+  const [weight, setWeight] = useState<number>(0);
+  const [date, setDate] = useState<string>((new Date()).toISOString())
 
   const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -11,7 +13,9 @@ function AddLift() {
       fetch("/create-lift", {
         method: "POST",
         body: JSON.stringify({
-          text: text,
+          reps: reps,
+          weight: weight,
+          date: date,
         }),
         headers: {
           'Content-Type': 'application/json'
@@ -20,8 +24,8 @@ function AddLift() {
       .then(res => {
         res.json()
         if (res.status === 200) {
-          setText('')
-          setMessage("User created successfully");
+          setReps(1);
+          setMessage("Lift added successfully");
         } else {
           setMessage("Some error occured");
         }
@@ -31,16 +35,40 @@ function AddLift() {
       console.log(err);
     }
   };
-  
+  //some helpers to map # of rep options
+  const repOptions = [];
+  for (let i=1;i < 16;i++) {
+    repOptions.push(i)
+  }
+  const mappedRepOptions = repOptions.map((i) => {
+    return(<option value={i}>{i}</option>)
+  })
 
   return(
     <div>
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+        <label>
+          Select number of Reps:
+          <select value={reps} onChange={(e) => setReps(Number(e.target.value))}>
+            {mappedRepOptions}
+          </select>
+        </label>
+        <label>
+          Enter the weight:
+          <input
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(Number(e.target.value))}
+          />
+        </label>
+        <label>
+          Enter the date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
         <input type="submit" value="Submit" />
       </form>
       <p>{message}</p>
