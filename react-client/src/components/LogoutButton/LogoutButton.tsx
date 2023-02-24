a1ec8290cdde6d5cc20dc47ccf73dc12f3ba52c2
= <=>
2023-02-24 14:17:43 -0700

    updated logout redirect url

--- react-client/src/components/LogoutButton/LogoutButton.tsx
+++ react-client/src/components/LogoutButton/LogoutButton.tsx
@@ -5,11 +5,18 @@ interface LogoutProps  {
   className: string
 }
 
+let redirect_url:string | undefined;
+if (process.env.NODE_ENV === 'development') {
+  redirect_url = window.location.origin;
+} else {
+  redirect_url = 'https://joshbroughton.github.io/pr-tracker-react/';
+}
+
 const LogoutButton = (props:LogoutProps) => {
   const { logout } = useAuth0();
 
   return (
-    <button className={props.className} onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <button className={props.className} onClick={() => logout({ logoutParams: { returnTo: redirect_url } })}>
       Log Out
     </button>
   );
