30cd46a552e835971b38fead392f3320eb0c2cd2
= <=>
2023-02-24 13:25:18 -0700

    setting up environment variables for database and server requests

--- express-server/db.js
+++ express-server/db.js
@@ -1,11 +1,21 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 // this is what connects express to the db
-const pool = new Pool({
-  user: 'postgres',
-  password: '',
-  host: 'localhost',
-  port: 5432,
-  database: 'prtracker'
-});
+let pool;
+// for localhost connections (development)
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+  pool = new Pool({
+    user: 'postgres',
+    password: '',
+    host: 'localhost',
+    port: 5432,
+    database: 'prtracker'
+  });
+} else { // gets the connection string from render.com environment variables
+  pool = new Pool({
+    connectionString: process.env.CONNECTION_STRING,
+  })
+}
+
 
 module.exports = pool;
